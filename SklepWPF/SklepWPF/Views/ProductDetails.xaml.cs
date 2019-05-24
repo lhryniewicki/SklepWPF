60abde698ad8dd36f6e24b7754238d8f904a7dba
Lukasz Hryniewicki <lukasz123-80@o2.pl>
2019-05-24 11:04:00 +0200

    Zebranie widokow

---
+++ SklepWPF/SklepWPF/Views/ProductDetails.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SklepWPF
+{
+	/// <summary>
+	/// Interaction logic for SzczegolyProduktu.xaml
+	/// </summary>
+	public partial class SzczegolyProduktu : Page
+	{
+		public SzczegolyProduktu()
+		{
+			InitializeComponent();
+		}
+	}
+}
